bbf511cfbdd548209b64fdaa3143733a71448a8f
Kevin Kipp <kevin.kipp@gmail.com>
2017-11-01 22:59:00 -0500

    Use file-loader with svg's

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.svg$/, loader: "file-loader" },
     ],
   },
   plugins: [
