6331725f651fe82078c834a694f62131449fa03d
Kevin Kipp <kevin.kipp@gmail.com>
2017-11-04 00:56:49 -0500

    Fix types in app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,15 @@ const focalPoints: FocalPoint[] = []
 
 Array.prototype.forEach.call(
   document.querySelectorAll(".focal-point"),
-  function(container) {
+  function(container: HTMLElement) {
     focalPoints.push(new FocalPoint(container))
   },
 )
 
-function updateFocalPoints(x, y) {
+function updateFocalPoints(x: number, y: number) {
   focalPoints.forEach(fp => {
-    fp.container.setAttribute("data-focus-x", x)
-    fp.container.setAttribute("data-focus-y", y)
+    fp.container.setAttribute("data-focus-x", x.toString())
+    fp.container.setAttribute("data-focus-y", y.toString())
     fp.adjustFocus()
   })
 }
@@ -23,5 +23,9 @@ const focalPicker = document.getElementById("focus-point-picker-img")
 if (focalPicker) {
   new FocalPicker(focalPicker as HTMLImageElement, {
     onUpdate: updateFocalPoints,
+    initialCoordinates: {
+      x: 0.75,
+      y: -0.25,
+    },
   })
 }
