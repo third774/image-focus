3fb1950c8521ebad87d4a712fc88ab25d31171d3
Kevin Kipp <kevin.kipp@gmail.com>
2017-11-05 11:03:09 -0600

    Add inputs to demo

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -6,21 +6,29 @@ Array.prototype.forEach.call(document.querySelectorAll(".focal-point"), function
   focalPoints.push(new FocalPoint(container))
 })
 
-function updateFocalPoints(x: number, y: number) {
+const coordinates = document.querySelector(".coordinates") as HTMLInputElement
+
+function updateCoordinates(x: number, y: number) {
   focalPoints.forEach(fp => {
     fp.container.setAttribute("data-focus-x", x.toString())
     fp.container.setAttribute("data-focus-y", y.toString())
     fp.adjustFocus()
   })
+
+  coordinates.value = `{x: ${x > 0 ? " " : ""}${x.toFixed(2)}, y: ${y > 0 ? " " : ""}${y.toFixed(2)}}`
 }
 
 const focalPickerEl = document.getElementById("focus-point-picker-img")
+let focalPicker: FocalPicker
 if (focalPickerEl) {
-  const focalPicker = new FocalPicker(focalPickerEl as HTMLImageElement, {
-    onUpdate: updateFocalPoints,
-    initialCoordinates: {
-      x: 0.75,
-      y: -0.25,
-    },
+  focalPicker = new FocalPicker(focalPickerEl as HTMLImageElement, {
+    onUpdate: updateCoordinates,
   })
 }
+
+const imgSrc = document.querySelector(".image-src") as HTMLInputElement
+
+imgSrc.addEventListener("input", function(e) {
+  focalPicker.img.src = imgSrc.value
+  focalPoints.forEach(fp => (fp.img.src = imgSrc.value))
+})
