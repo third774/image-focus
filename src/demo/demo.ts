67a3a2f40afd30ec7e71811ebf977d93bcb38754
Kevin Kipp <kevin.kipp@gmail.com>
2017-11-05 21:15:37 -0600

    update demo

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -2,32 +2,32 @@ import { ImageFocus, FocusPicker, initImageFocus } from "../lib/main"
 
 const images: ImageFocus[] = []
 
+const startingFocus = { x: 0.81, y: -0.69 }
+
 Array.prototype.forEach.call(document.querySelectorAll(".image-focus"), function(container: HTMLElement) {
   images.push(
     new ImageFocus(container, {
-      focus: { x: 0.81, y: -0.69 },
+      focus: startingFocus,
     }),
   )
 })
 
 const coordinates = document.querySelector(".coordinates") as HTMLInputElement
 
-function updateCoordinates(x: number, y: number) {
-  images.forEach(i => {
-    i.setFocus(x, y)
-  })
-
-  coordinates.value = `{x: ${x > 0 ? " " : ""}${x.toFixed(2)}, y: ${y > 0 ? " " : ""}${y.toFixed(2)}}`
+function updateCoordinatesValue({ x, y }: { x: number; y: number }) {
+  coordinates.value = `{x: ${x >= 0 ? " " : ""}${x.toFixed(2)}, y: ${y >= 0 ? " " : ""}${y.toFixed(2)}}`
 }
+
+updateCoordinatesValue(startingFocus)
 
 const focusPickerEl = document.getElementById("image-focus-picker-img")
-let focusPicker: FocusPicker
-if (focusPickerEl) {
-  focusPicker = new FocusPicker(focusPickerEl as HTMLImageElement, {
-    onChange: updateCoordinates,
-    focus: { x: 0.81, y: -0.69 },
-  })
-}
+const focusPicker = new FocusPicker(focusPickerEl as HTMLImageElement, {
+  onChange: (x: number, y: number) => {
+    images.forEach(i => i.setFocus(x, y))
+    updateCoordinatesValue({ x, y })
+  },
+  focus: startingFocus,
+})
 
 const imgSrc = document.querySelector(".image-src") as HTMLInputElement
 
