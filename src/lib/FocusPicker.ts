af6d6ebb49723ca3577d1f3deab28c350b1cacce
Kevin Kipp <kevin.kipp@gmail.com>
2017-11-08 22:47:13 -0600

    Support touch and custom retina

--- src/lib/FocusPicker.ts
+++ src/lib/FocusPicker.ts
@@ -20,27 +20,39 @@ const CONTAINER_STYLES = {
 const RETINA_STYLES = {
   position: "absolute",
   cursor: "move",
+  transform: "translate(-50%, -50%)",
 }
 
 const DEFAULT_OPTIONS: FocusPickerOptions = {
   onChange: noop,
+  retina,
 }
 
 export interface FocusPickerOptions {
+  /**
+   * callback for when the focus point changes
+   */
   onChange?: (x: number, y: number) => void
+  /**
+   * focus point to initialize with
+   */
   focus?: {
     x: number
     y: number
   }
+  /**
+   * src attribute for the retina
+   */
+  retina?: string
 }
 
 export class FocusPicker {
   container: HTMLElement
   img: HTMLImageElement
   retina: HTMLImageElement
-  isDragging: boolean
   focusX: number
   focusY: number
+  private isDragging: boolean
   private options: FocusPickerOptions
 
   constructor(initializationNode: HTMLImageElement, options: FocusPickerOptions) {
@@ -50,16 +62,22 @@ export class FocusPicker {
     this.setUpImageAttributes()
     this.assignStyles()
     this.initailizeFocusCoordinates()
-    setTimeout(() => this.updateRetinaPosition(this.calculateOffsetFromFocus()), 0)
+    setTimeout(() => this.updateRetinaPositionFromFocus(), 0)
   }
 
-  assignStyles() {
+  public setFocus = (x, y) => {
+    this.focusX = x
+    this.focusY = y
+    this.updateRetinaPositionFromFocus()
+  }
+
+  private assignStyles() {
     assignStyles(this.img, IMAGE_STYLES)
     assignStyles(this.retina, RETINA_STYLES)
     assignStyles(this.container, CONTAINER_STYLES)
   }
 
-  initailizeFocusCoordinates() {
+  private initailizeFocusCoordinates() {
     this.focusX = firstNumberIn([
       this.options.focus && this.options.focus.x,
       this.img.getAttribute("data-focus-x"),
@@ -73,80 +91,84 @@ export class FocusPicker {
     ])
   }
 
-  setUpElementReferences(initializationNode: HTMLElement | HTMLImageElement) {
-    if (initializationNode.nodeName === "IMG") {
-      this.img = initializationNode as HTMLImageElement
-      this.container = initializationNode.parentElement as HTMLElement
-    } else {
-      this.container = initializationNode as HTMLElement
-      this.img = initializationNode.querySelector("img") as HTMLImageElement
-      if (!this.img) {
-        console.error(initializationNode)
-        throw new Error("No image found within above container")
-      }
-    }
+  private setUpElementReferences(initializationNode: HTMLImageElement) {
+    this.img = initializationNode
+    this.container = initializationNode.parentElement
     this.retina = document.createElement("img")
-    this.retina.src = retina
+    this.retina.src = this.options.retina
     this.retina.draggable = false
     this.container.appendChild(this.retina)
   }
 
-  bindContainerEvents() {
-    this.container.onmousedown = this.startDragging
-    this.container.onmouseup = this.stopDragging
-    this.container.onmouseleave = this.stopDragging
-    this.container.onmousemove = this.handleDrag
+  private bindContainerEvents() {
+    this.container.addEventListener("mousedown", this.startDragging)
+    this.container.addEventListener("touchstart", this.startDragging)
+    this.container.addEventListener("mousemove", this.handleMove)
+    this.container.addEventListener("touchmove", this.handleMove)
+    this.container.addEventListener("mouseup", this.stopDragging)
+    this.container.addEventListener("mouseleave", this.stopDragging)
+    this.container.addEventListener("touchend", this.stopDragging)
   }
 
-  setUpImageAttributes() {
-    this.img.draggable = false
-    this.img.onload = () => {
-      this.updateRetinaPosition(this.calculateOffsetFromFocus())
+  private startDragging = (e: MouseEvent | TouchEvent) => {
+    this.isDragging = true
+    e instanceof MouseEvent
+      ? this.updateCoordinates(e.clientX, e.clientY)
+      : this.updateCoordinates(e.touches[0].clientX, e.touches[0].clientY)
+  }
+
+  private handleMove = (e: MouseEvent | TouchEvent) => {
+    e.preventDefault()
+    if (e instanceof MouseEvent) {
+      this.updateCoordinates(e.clientX, e.clientY)
+    } else {
+      const touch = e.touches[0]
+      const touchedEl = document.elementFromPoint(touch.pageX, touch.pageY)
+      touchedEl !== this.retina && touchedEl !== this.img
+        ? this.stopDragging()
+        : this.updateCoordinates(touch.clientX, touch.clientY)
     }
   }
 
-  calculateOffsetFromFocus = () => {
+  private stopDragging = () => {
+    this.isDragging = false
+  }
+
+  private setUpImageAttributes() {
+    this.img.draggable = false
+    this.img.addEventListener("load", this.updateRetinaPositionFromFocus)
+  }
+
+  private calculateOffsetFromFocus = () => {
     const { width, height } = this.img.getBoundingClientRect()
     const offsetX = width * (this.focusX / 2 + 0.5)
     const offsetY = height * (this.focusY / -2 + 0.5)
     return { offsetX, offsetY }
   }
 
-  updateRetinaPosition = (offsets: { offsetX: number; offsetY: number }) => {
-    this.retina.style.top = `calc(${offsets.offsetY}px - 10px)`
-    this.retina.style.left = `calc(${offsets.offsetX}px - 10px)`
-  }
-
-  setFocus = (x, y) => {
-    this.focusX = x
-    this.focusY = y
+  private updateRetinaPositionFromFocus = () => {
     this.updateRetinaPosition(this.calculateOffsetFromFocus())
   }
 
-  startDragging = (e: MouseEvent) => {
-    this.isDragging = true
-    this.handleDrag(e)
+  private updateRetinaPosition = (offsets: { offsetX: number; offsetY: number }) => {
+    this.retina.style.top = `${offsets.offsetY}px`
+    this.retina.style.left = `${offsets.offsetX}px`
   }
 
-  stopDragging = () => {
-    this.isDragging = false
-  }
+  private updateCoordinates(clientX: number, clientY: number) {
+    if (!this.isDragging) return // bail if not dragging
+    const { width, height, left, top } = this.img.getBoundingClientRect()
 
-  handleDrag = (e: MouseEvent) => {
-    if (this.isDragging) {
-      const { width, height, left, top } = this.img.getBoundingClientRect()
+    // Calculate FocusPoint coordinates
+    const offsetX = clientX - left
+    const offsetY = clientY - top
+    const focusX = (offsetX / width - 0.5) * 2
+    const focusY = (offsetY / height - 0.5) * -2
 
-      // Calculate FocusPoint coordinates
-      const offsetX = e.clientX - left
-      const offsetY = e.clientY - top
-      const focusX = (offsetX / width - 0.5) * 2
-      const focusY = (offsetY / height - 0.5) * -2
+    this.updateRetinaPosition({ offsetX, offsetY })
 
-      this.updateRetinaPosition({ offsetX, offsetY })
-
-      this.img.setAttribute("data-focus-x", focusX.toString())
-      this.img.setAttribute("data-focus-y", focusY.toString())
-      this.options.onChange(focusX, focusY)
-    }
+    this.img.setAttribute("data-focus-x", focusX.toString())
+    this.img.setAttribute("data-focus-y", focusY.toString())
+    this.options.onChange(focusX, focusY)
   }
 }
