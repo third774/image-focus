d7e741bec0bf943e863796d609478e557f7e78db
Kevin Kipp <kevin.kipp@gmail.com>
2017-11-09 00:14:22 -0600

    Export assign function

--- src/lib/helpers/assign.ts
+++ src/lib/helpers/assign.ts
@@ -1,4 +1,4 @@
-function assign(target: any, ...sources) {
+export function assign(target: any, ...sources) {
   sources.reverse().forEach(source => Object.keys(source).forEach(key => (target[key] = source[key])))
   return target
 }
