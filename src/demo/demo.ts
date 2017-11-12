b60b87b589c6326f64aa35bd20047df90984c14f
Kevin Kipp <kevin.kipp@gmail.com>
2017-11-12 10:25:56 -0600

    Add data attributes display

--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,37 +1,32 @@
 import { FocusedImage, FocusPicker, Focus } from "../lib/main"
 
-const focusedImages: FocusedImage[] = []
-
-const startingFocus = { x: 0, y: 0 }
-
-Array.prototype.forEach.call(document.querySelectorAll(".focused-image"), (imageEl: HTMLImageElement) => {
-  focusedImages.push(
-    new FocusedImage(imageEl, {
-      focus: startingFocus,
-    }),
-  )
-})
-
-const coordinates = document.getElementById("coordinates") as HTMLInputElement
-
-function updateCoordinatesValue({ x, y }: Focus) {
-  coordinates.value = `{x: ${x >= 0 ? " " : ""}${x.toFixed(2)}, y: ${y >= 0 ? " " : ""}${y.toFixed(2)}}`
-}
-
-updateCoordinatesValue(startingFocus)
-
 const focusPickerEl = document.getElementById("focus-picker-img") as HTMLImageElement
-const focusPicker = new FocusPicker(focusPickerEl, {
-  onChange: focus => {
-    focusedImages.forEach(focusedImage => focusedImage.setFocus(focus))
-    updateCoordinatesValue(focus)
-  },
-  focus: startingFocus,
+const imgSrcEl = document.getElementById("image-src") as HTMLInputElement
+const coordinates = document.getElementById("coordinates") as HTMLInputElement
+const dataAttributes = document.getElementById("data-attributes") as HTMLInputElement
+const focusedImageElements = document.querySelectorAll(".focused-image") as NodeListOf<HTMLImageElement>
+
+// Set our starting point
+const focus = { x: 0.75, y: 0 }
+
+//
+const focusedImages: FocusedImage[] = []
+Array.prototype.forEach.call(focusedImageElements, (imageEl: HTMLImageElement) => {
+  focusedImages.push(new FocusedImage(imageEl, { focus }))
 })
 
-const imgSrcEl = document.getElementById("image-src") as HTMLInputElement
+const focusPicker = new FocusPicker(focusPickerEl, {
+  focus,
+  onChange: (newFocus: Focus) => {
+    const x = newFocus.x.toFixed(2)
+    const y = newFocus.y.toFixed(2)
+    coordinates.value = `{x: ${x}, y: ${y}}`
+    dataAttributes.value = `data-focus-x="${x}" data-focus-y="${y}"`
+    focusedImages.forEach(focusedImage => focusedImage.setFocus(newFocus))
+  },
+})
 
 imgSrcEl.addEventListener("input", function(e) {
   focusPicker.img.src = imgSrcEl.value
-  focusedImages.forEach(fp => (fp.img.src = imgSrcEl.value))
+  focusedImages.forEach(focusedImage => (focusedImage.img.src = imgSrcEl.value))
 })
